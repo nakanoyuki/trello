66061bb9dac19e655d1240273fda9209a71c4209
nakanoyuki <yukich41@gmail.com>
2022-12-01 14:29:31 +0900

    TaskCardsコンポーネントの作成

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Header from "./components/atoms/layout/Header";
+import TaskCards from "./components/task/TaskCards";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +10,7 @@ function App() {
   return (
     <div>
       <Header />
+      <TaskCards />
     </div>
   );
 }
