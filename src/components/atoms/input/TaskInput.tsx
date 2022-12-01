d0e298e294e134e5eb5d237193273d460a54cb78
nakanoyuki <yukich41@gmail.com>
2022-12-01 16:25:39 +0900

    Typescript型定義

--- src/components/atoms/input/TaskInput.tsx
+++ src/components/atoms/input/TaskInput.tsx
@@ -1,7 +1,38 @@
-import React from "react";
+import { Dispatch, SetStateAction } from "react";
+import { useState } from "react";
 
-const TaskInput = () => {
-  return <div></div>;
+type Props = {
+  inputText: string;
+  setInputText: Dispatch<SetStateAction<string>>;
+  taskList: string[];
+  setTaskList: Dispatch<SetStateAction<string[]>>;
+};
+
+const TaskInput = ({
+  inputText,
+  setInputText,
+  taskList,
+  setTaskList,
+}: Props) => {
+  const onHandleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+  const onHandleInputText = (e: React.ChangeEvent<HTMLInputElement>): void => {
+    setInputText(e.target.value);
+    // console.log(inputText);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onHandleSubmit}>
+        <input
+          type="text"
+          placeholder="タスク追加"
+          onChange={onHandleInputText}
+        />
+      </form>
+    </div>
+  );
 };
 
 export default TaskInput;
--- src/components/task/TaskCard.tsx
+++ src/components/task/TaskCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import TaskDeleteButton from "../atoms/button/TaskDeleteButton";
 import TaskInput from "../atoms/input/TaskInput";
@@ -6,21 +6,26 @@ import TaskCardTitle from "./card/TaskCardTitle";
 import Tasks from "./card/Tasks";
 
 const TaskCard = () => {
+  const [inputText, setInputText] = useState<string>("");
+  const [taskList, setTaskList] = useState<string[]>([]);
   return (
     <STaskCard>
       <TaskCardTitle />
       <TaskDeleteButton />
       <Tasks />
-      <TaskInput />
+      <TaskInput
+        inputText={inputText}
+        setInputText={setInputText}
+        taskList={taskList}
+        setTaskList={setTaskList}
+      />
     </STaskCard>
   );
 };
-
 const STaskCard = styled.div`
   width: 400px;
   background: #f7f7f7;
   border-radius: 10px;
   padding: 20px 20px 26px;
 `;
-
 export default TaskCard;
